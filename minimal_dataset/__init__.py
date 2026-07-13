58e3bf4a20e0fea6894d65c7af9c5d2cc2cd407f
Pascal Nague <pana01@dfki.de>
2026-07-13 04:57:05 +0200

    Add BaseDataset and __init__: complete minimal_dataset module

---
+++ minimal_dataset/__init__.py
@@ -0,0 +1,14 @@
+"""
+Minimal Dependency Dataset Library for PyTorch.
+
+Usage:
+    from minimal_dataset import BaseDataset, DataLoader
+
+    dataset = BaseDataset("/path/to/data")
+    loader = DataLoader(dataset, batch_size=32, num_workers=4)
+    for images, labels in loader:
+        # train...
+"""
+from .dataset import BaseDataset
+from .sampler import LockFreeSampler
+from .dataloader import DataLoader
